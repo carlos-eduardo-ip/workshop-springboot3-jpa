993627cc7f3c3475516047d7e28bb35b5f0d6bfe
Carlos Eduardo <carlos.eduardog2p@gmail.com>
2024-01-21 22:19:57 -0300

    H2 database, test profile, JPA

--- src/main/java/com/capacity/The_Backupers/entities/User.java
+++ src/main/java/com/capacity/The_Backupers/entities/User.java
@@ -1,11 +1,16 @@
 package com.capacity.The_Backupers.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
-
+@Entity
+@Table(name = "tb-user")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
