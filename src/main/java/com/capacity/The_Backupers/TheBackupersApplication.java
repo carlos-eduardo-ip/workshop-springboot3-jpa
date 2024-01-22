055bbbbb405ec0f448b9110cb05c079b510e293d
Carlos Eduardo <carlos.eduardog2p@gmail.com>
2024-01-21 21:25:20 -0300

    Project created

---
+++ src/main/java/com/capacity/The_Backupers/TheBackupersApplication.java
@@ -0,0 +1,13 @@
+package com.capacity.The_Backupers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBackupersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBackupersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capacity/The_Backupers/TheBackupersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capacity.The_Backupers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBackupersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
