ecc9e660af2981302bc7c0bc1a4b33cb60bf3a5f
Carlos Eduardo <carlos.eduardog2p@gmail.com>
2024-02-18 22:10:35 -0300

    Payment, one-to-one association

--- src/main/java/com/capacity/The_Backupers/entities/Order.java
+++ src/main/java/com/capacity/The_Backupers/entities/Order.java
@@ -26,9 +26,11 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private  User client;
 
-
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order(){}
 
@@ -75,6 +77,14 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItems(){
         return items;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
